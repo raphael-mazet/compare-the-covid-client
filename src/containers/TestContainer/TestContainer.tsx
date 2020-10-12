70791fe26294e93ab48da5d5c6586944fa3ab420
Raphael <raphael.mazet@gmail.com>
2020-10-12 13:02:49 +0100

    feature: add working test get query from server with apollo client

--- src/containers/TestContainer/TestContainer.tsx
+++ src/containers/TestContainer/TestContainer.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+const testQuery = gql `
+  query getRaph {
+    getUserbyId(id:1) {
+      username
+    }
+  }
+`
+
+interface Data {
+  getUserbyId: {
+    id: number;
+    username: string;
+    password: string;
+    email: string
+  }
+}
+
+interface Variables {
+  first: number;
+}
 
 export default function TestContainer(): JSX.Element {
+  const {data, loading, error} = useQuery<Data>(testQuery)
+  
   return (
   <>
-  Hello
+    <div>
+      {loading ? <p>loading</p> : <p>Hello {data?.getUserbyId.username}</p> }
+    </div>
   </>
   )
 }
