064aeb2d894b446c9b01cf2257d9f6519669c79d
Raphael <raphael.mazet@gmail.com>
2020-10-12 16:07:06 +0100

    typescript: add corrent types on handleSubmit and handleChange in test container

--- src/containers/TestContainer/TestContainer.tsx
+++ src/containers/TestContainer/TestContainer.tsx
@@ -1,26 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 
 const testQuery = gql `
   query getRaph {
-    getUserbyId(id:1) {
+    getUserbyId(id:4) {
       username
     }
   }
 `
 
-interface Data {
+interface queryData {
   getUserbyId: {
     id: number;
     username: string;
     password: string;
     email: string
   }
-}
-
-interface Variables {
-  first: number;
 }
 
 interface newUser {
@@ -30,29 +26,52 @@ interface newUser {
   email: string
 }
 
-export default function TestContainer(): JSX.Element {
-  const {data, loading, error} = useQuery<Data>(testQuery)
-  const [newUser, setNewUser] = useState<newUser | undefined>(undefined);
+const CREATE_USER = gql `
+mutation CreateUser($username: String!, $email: String!, $password: String!) {
+	createUser(username:$username, password: $password, email:$email ) {
+    username
+  }
+}
+`
 
-  function handleChange(e: any) {
+export default function TestContainer(): JSX.Element {
+  const [newUser, setNewUser] = useState<newUser | undefined>(undefined);
+  const {data: userData, loading, error: userDataError} = useQuery<queryData>(testQuery)
+  const [saveUser, { data: savedUserData, error: saveUserError }] = useMutation<
+    {createUser: newUser},
+    {
+      username: string | undefined,
+      password: string | undefined,
+      email: string | undefined
+    }
+  >(CREATE_USER, {
+    variables: {
+      username: newUser?.username,
+      password: newUser?.password,
+      email: newUser?.email
+    }
+
+  })
+
+  //install ts lint? extension 
+  function handleChange(e: ChangeEvent<HTMLInputElement>): void {
     e.persist();
     setNewUser((newUser: any) => (
       {...newUser, [e.target.id]: e.target.value}
     ))
   }
 
-  function handleSubmit(e: any) {
-    e.precentDefault();
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    saveUser()
   }
   
   return (
   <>
     <div>
-      {loading ? <p>loading</p> : <p>Hello {data?.getUserbyId.username}</p> }
+      {loading ? <p>loading</p> : <p>Hello {userData?.getUserbyId.username}</p> }
     </div>
     <form onSubmit={handleSubmit}>
-      <label htmlFor="id">id</label>
-      <input id="id" type="text" name="id" value={newUser?.id} onChange={handleChange}/>
       <label htmlFor="username">username</label>
       <input id="username" type="text" name="username" value={newUser?.username} onChange={handleChange}/>
       <label htmlFor="password">password</label>
@@ -61,6 +80,10 @@ export default function TestContainer(): JSX.Element {
       <input id="email" type="text" name="email" value={newUser?.email} onChange={handleChange}/>
       <button type="submit">Submit</button>
     </form>
+    <div>
+      {savedUserData && <p>You have created {userData?.getUserbyId.username}</p> }
+    </div>
+
   </>
   )
 }
