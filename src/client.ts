1b2fdc93a1937d3d66e00d819abd76875f1be65f
Raphael <raphael.mazet@gmail.com>
2020-10-13 10:09:32 +0100

    api: update Test Container with direct read from cache example

--- src/client.ts
+++ src/client.ts
@@ -4,7 +4,6 @@ import {
   HttpLink,
   // NormalizedCacheObject,
 } from '@apollo/client';
-import gql from 'graphql-tag';
 
 const link = new HttpLink({uri: 'http://localhost:4000/'})
 const cache = new InMemoryCache();
@@ -12,18 +11,6 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
   link, cache
 });
-
-// const query = gql`
-//   {
-//     characters {
-//       results {
-//         name
-//       }
-//     }
-//   }
-// `
-// client.query({query})
-//   .then(result => console.log(result))
 
 
 export default client;
--- src/containers/TestContainer/TestContainer.tsx
+++ src/containers/TestContainer/TestContainer.tsx
@@ -3,6 +3,7 @@ import { useQuery, useMutation } from '@apollo/react-hooks';
 import { User, QueryData } from '../../interfaces/query.interface'
 import { GET_USER_BY_ID } from '../../apis/graphQL/queries/getUsers'
 import { CREATE_USER } from '../../apis/graphQL/mutations/createUser'
+import client from '../../client'
 
 export default function TestContainer(): JSX.Element {
   const [newUser, setNewUser] = useState<User>({
@@ -12,20 +13,34 @@ export default function TestContainer(): JSX.Element {
     password: ''
   });
 
+  //NOTE: useQuery hook example - run immediately when called. Pulls from server
   const {data: userData, loading, error: userDataError} = useQuery<QueryData>(GET_USER_BY_ID, {
     variables: {
       id:3
     }
   })
+
+  //NOTE: useMutation hook example: saveUser needs to be called explicitly in order to run
   const [saveUser, { data: savedUserData, error: savedUserError }] = useMutation<{createUser: User}>(
     CREATE_USER, {
     variables: {
       username: newUser?.username,
       password: newUser?.password,
       email: newUser?.email
-    //NOTE: update(cache,...) also comes here
+    //NOTE: if an update cached operation is needed, it comes here: https://www.apollographql.com/docs/react/data/mutations/
     }
   })
+
+//NOTE: example of the client reading from cache instead of server, re-using the initial query name
+const cacheRead = client.readQuery({
+  query: GET_USER_BY_ID
+  ,
+  variables: {
+    id: 3,
+  },
+})
+console.log('cacheRead --->', cacheRead);
+
 
   function handleChange(e: ChangeEvent<HTMLInputElement>){
     e.persist();
