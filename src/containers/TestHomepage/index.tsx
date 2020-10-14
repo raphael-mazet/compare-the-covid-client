b66c6a6e7abd13b001d87ac49a03073104a240e1
Raphael <raphael.mazet@gmail.com>
2020-10-14 22:04:50 +0100

    render green, yellow, red

--- src/containers/TestHomepage/index.tsx
+++ src/containers/TestHomepage/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import client from '../../client';
 import { GET_USER_BY_ID, GET_EVENTS_BY_MULTIPLE_LOCATION_IDS } from '../../apis/graphQL/queries/index';
-import { User } from '../../interfaces/query.interface';
+import { User, Event } from '../../interfaces/query.interface';
 
 const Homepage: React.FunctionComponent = () => {
 
@@ -13,6 +13,12 @@ const Homepage: React.FunctionComponent = () => {
     lastName: ''
   })
 
+  const [sessionEvents, setSessionEvents] = useState<any>({
+    green: [],
+    yellow: [],
+    red: []
+  })
+
   useEffect (() => {
   const user = client.readQuery({
     query: GET_USER_BY_ID,
@@ -22,18 +28,35 @@ const Homepage: React.FunctionComponent = () => {
   })
   setSessionUser(user.getUserbyId);
 
-  // const sessionEvents = client.readQuery({
-  //   query: GET_EVENTS_BY_MULTIPLE_LOCATION_IDS,
-  //   variables: {
-  //     id: [1,2],
-  //   },
-  // })
-  // console.log('sessionEvents --->', sessionEvents);
+  const events = client.readQuery({
+    query: GET_EVENTS_BY_MULTIPLE_LOCATION_IDS,
+    variables: {
+      location_ids: [1,2,3,4],
+    },
+  })
+
+  
+    const tempEvents: any = {
+      green: [],
+      yellow: [],
+      red: []
+    }
+  events.getEventsbyMultipleLocationIds.forEach((event: Event) => {
+    if (event.alertType === 'confirmed') {
+      tempEvents.red.push(event)
+    } else if (event.alertType === 'suspected') {
+      tempEvents.yellow.push(event)
+    } else if (event.alertType === 'safe') {
+      tempEvents.green.push(event)
+    }
+  })
+  setSessionEvents(tempEvents)
   }, [])
 
   return (
     <>
       <p> Hello{sessionUser.id ? ` ${sessionUser.firstName}` : ''} </p>
+      <p>You have {`${sessionEvents.green.length} green, ${sessionEvents.yellow.length} yellow, ${sessionEvents.red.length} red`}</p>
     </>
   );
 };
