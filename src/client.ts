f3331e782a1488f242374f30b4d407e698cf209e
David Hardy <david.hardy50@gmail.com>
2020-10-15 19:58:20 +0200

    feat: searchbar updating local state

--- src/client.ts
+++ src/client.ts
@@ -2,15 +2,46 @@ import {
   ApolloClient,
   InMemoryCache,
   HttpLink,
-  // NormalizedCacheObject,
+  makeVar,
 } from "@apollo/client";
 import { resolvers, typeDefs } from './apis/graphQL/resolvers';
 import { gql } from '@apollo/client';
 
+interface Geometry {
+  id?: number,
+  latitude: number,
+  longitude: number,
+}
+
+const geometrysInitialValue: Geometry = 
+  {
+    id: 0,
+    latitude: 500,
+    longitude: 600,
+  }
+
+export const geometrysVar = makeVar<Geometry>(
+  geometrysInitialValue
+)
+
+export const cache: InMemoryCache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        searchdata: {
+          read () {
+            return geometrysVar();
+          }
+        }
+      }
+    }
+  }
+});
+
+
 const link = new HttpLink({
   uri: "http://localhost:4000",
 });
-const cache = new InMemoryCache();  
 
 const client = new ApolloClient({
   link,
@@ -18,6 +49,7 @@ const client = new ApolloClient({
   typeDefs,
   resolvers
 });
+
 
 //TODO: delete hard-coded id
 const myQuery = gql `
--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
+import Searchbar from '../../components/Searchbar'
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client'
+import { geometrysVar } from '../../client'
 
 const Homepage: React.FunctionComponent = () => {
+
+  const [queryMaps, setQueryMaps] = useState<any | (() => any)>("");
+  const [searchValue, setSearchValue] = useState<any | (() => any)>("");
+
+  let lat = null;
+  let lng = null;
+
+  if (queryMaps.geometry !== undefined) lat = queryMaps.geometry.location.lat();
+  if (queryMaps.geometry !== undefined) lng = queryMaps.geometry.location.lng();
+
+  const obj = {
+    id: 1,
+    latitude: lat,
+    longitude: lng,
+  }
+  
+  geometrysVar(obj)
+
+  console.log('lat', lat)
+  console.log('lng', lng)
+
+//   const geometryQuery = gql `
+//   query GetGeometry {
+//     geometrys @client {
+//       id 
+//       latitude
+//       longitude
+//     }
+//   }
+// `;
+
+// const { data } = useQuery(geometryQuery) 
+const data2 = geometrysVar()
+
   return (
     <>
-      <p> Homepage </p>
+      <Searchbar 
+        placeholder="Search a location"
+        inputAction={setQueryMaps}
+        searchValue={searchValue}
+        setSearch={setSearchValue}
+      />
+      <div>
+        {/* <p>{data.geometrys.id}</p>
+        <p>{data.geometrys.latitude}</p>
+        <p>{data.geometrys.longitude}</p> */}
+        <p>{data2.latitude}</p>
+        <p>{data2.longitude}</p>
+      </div>  
     </>
   );
 };
