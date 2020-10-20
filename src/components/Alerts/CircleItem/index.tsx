5fac54755b86e250a2ff864452b6b279e8a386a3
David Hardy <david.hardy50@gmail.com>
2020-10-20 22:35:02 +0200

    fix: remove link in circleitem and replace with usehistory

--- src/components/Alerts/CircleItem/index.tsx
+++ src/components/Alerts/CircleItem/index.tsx
@@ -2,26 +2,31 @@ import React from "react";
 import { circleItemProps } from "./index.interface";
 import "./index.style.scss";
 import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 
-const circleItem = ({alerts}: circleItemProps): JSX.Element | null => {
+const CircleItem = ({alerts}: circleItemProps): JSX.Element | null => {
   const { alertType, alertNumber, isNew } = alerts;
+
+  const history = useHistory();
 
   const displayText = (!isNew) ? 
   `You have no new cases and ${alertNumber} active case${(alertNumber === 1) ? '' : 's'} from the last week` :
   `You have ${alertNumber} new ${alertType} case${(alertNumber === 1) ? '' : 's'} since you last checked`;
 
+  const clickHandler = () => {
+    history.push('/alerts')
+  }
+
     return (
       <div className="circleContainer">
-        <Link to="/alerts">
-          <div className={`${alertType}-${isNew ? 'un' : ''}checked-styles`}>
-            <p className="number">{alertNumber}</p>
-          </div>
-        </Link>
-        <div>
+        <div onClick={clickHandler} className={`${alertType}-${isNew ? 'un' : ''}checked-styles`}>
+          <p className="number">{alertNumber}</p>
+        </div>
+        <div className='alert_text'>
           <p>{displayText}</p>
         </div>
       </div>
     );
 };
 
-export default circleItem;
+export default CircleItem;
