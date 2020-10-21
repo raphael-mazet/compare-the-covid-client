3279332ba34d08115ca63ae5e3df888f62634862
filvecchiato <fil.vecchiato@gmail.com>
2020-10-21 12:39:28 +0100

    fix: fixed string editing

--- src/components/LocationInformation/index.tsx
+++ src/components/LocationInformation/index.tsx
@@ -19,14 +19,14 @@ const LocationInfo = (props: propTypes): JSX.Element => {
     data
   } = props;
   
-  data.location_type = data.location_type.split('_').join(' ');
+  const locationType = data.location_type.replace('_', ' ');
   
   return (  
     <div className='container'>
       <div>
         <p className="location_info_name">{data.name}</p>
         {/* <p className="location_info_data">lat: {parseFloat(data.latitude).toFixed(3)} - long: {parseFloat(data.longitude).toFixed(3)} </p> */}
-        <p className="location_info_data">{data.location_type}</p>
+        <p className="location_info_data">{locationType}</p>
       </div>
     </div>
   );
