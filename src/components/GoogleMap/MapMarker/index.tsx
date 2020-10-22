d114b33d30668b1c79f602257b098ef3eaa29017
David Hardy <david.hardy50@gmail.com>
2020-10-22 12:36:07 +0200

    map marker logo

--- src/components/GoogleMap/MapMarker/index.tsx
+++ src/components/GoogleMap/MapMarker/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./index.style.scss";
 import alert from '../../../images/alert.png'
+import man from '../../../images/man.png'
 
 interface MapMarkerProps {
   lat: number
@@ -17,7 +18,7 @@ const MapMarker = ({ lat, lng, text, onClick }: MapMarkerProps): JSX.Element => 
       <div className={['wrapper', currentLocation].join(' ')}
         onClick={onClick}
       >
-        {text !== 'Current location' ? <img className='alert_icon' src={alert} alt='alerticon'/> : null}
+        {text !== 'Current location' ? <img className='alert_icon' src={alert} alt='alerticon'/> : <img className='alert_icon' src={man} alt='alerticon'/>}
       </div>
       <div className='text'>
         <p>{text}</p>
